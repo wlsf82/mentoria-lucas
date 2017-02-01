934bcbd479c881d339c0697d5c4a366732430675
ldasilvaNZ11 <lucas.cordeiro@gmail.com>
2017-02-01 17:34:48 +1300

    checks and keyword search on homepage
    I cannot get the keyword search scenario to work

--- tm-spec.js
+++ tm-spec.js
@@ -1,19 +1,42 @@
+//tm-spec.js
 
+//the command below allows protractor to test non-AngularJS webpages
 browser.ignoreSynchronization = true;
+
 describe('TM front-page tests', function() {
 	it('TM front-page should have a logo', function () {
-//arrange
+
+//ARRANGE
 
 		browser.get('http://www.trademe.co.nz/');
 	
-//act
-//var tmLogo = element(by.id('SiteHeader_SiteTabs_kevin'));
-var tmLogo = $ ('#SiteHeader_SiteTabs_kevin');
+//ACT
 
-//verify
-expect(tmLogo.isDisplayed()).toBeTruthy();
+//var tmLogo = element(by.id('SiteHeader_SiteTabs_kevin'));
+        var tmLogo = $('#SiteHeader_SiteTabs_kevin');
+
+
+//ASSERT
+		expect(tmLogo.isDisplayed()).toBeTruthy();
 
     });
+
+it ("Performing a keyword search", function() {
+    browser.get('http://www.trademe.co.nz/');
+
+    var searchBox = element(by.id('searchString'));
+    var searchResult = element(by.css('inline-block'));
+
+    searchBox.sendKeys('nike');
+    searchBox.sendKeys(protractor.Key.ENTER)
+
+    //expect(searchResult.getText()).toContain('Search results for \'nike\'');
+    expect(browser.getCurrentUrl()).toBe('http://www.trademe.co.nz/Browse/SearchResults.aspx?searchString=nike&type=Search&searchType=all&user_region=100&user_district=0&generalSearch_keypresses=4&generalSearch_suggested=0&generalSearch_suggestedCategory=');
+    
+});
+
+
+
 
 });
 
--- tm_conf.js
+++ tm_conf.js
@@ -1,5 +1,8 @@
+//tm_conf.js
+
 exports.config = {
-	seleniumAddress: 'http://localhost:4444/wd/hub',
+	//seleniumAddress: 'http://localhost:4444/wd/hub',
+	directConnect: true,
 	
 	specs: ['tm-spec.js'],
 	
